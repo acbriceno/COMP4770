5cb39ed7d0dbc4feb1fbca916a0a99a124efdbef
Matt Gilchrist <mag@Matthews-MacBook-Air.local>
2019-03-11 16:20:00 -0230

    updates to index and level editor

--- client/js/levelEditor.js
+++ client/js/levelEditor.js
@@ -1,15 +1,5 @@
+let mousePos;
 
-// let a1 = document.getElementById('selections');
-//
-// a1.onclick = function() {
-//       //let sel = a1.options[a1.selectedIndex].value;
-//       console.log("SEL");
-// }
-
-/*let a1 = document.getElementById('selections');
-
-a1.onclick = function() {
-      //let sel = a1.options[a1.selectedIndex].value;
-      console.log("SEL");
+MousePos = function(x, y) {
+      let self ={x:x, y:y};
 }
-*/
