d60ce33657ecd5358c326d541b4fc9cf17204cf4
Matt Gilchrist <mag@Matthews-MacBook-Air.local>
2019-03-13 15:01:29 -0230

    confilcts

--- client/js/levelEditor.js
+++ client/js/levelEditor.js
@@ -1,28 +1,20 @@
 let mousePos;
 let currentLevel;
 
-<<<<<<< HEAD
-MousePos = function(x, y) {
-      let self ={x:x, y:y};
-
-      self.getCoord = function() {
-            return(self.x, self.y)
-      }
-=======
 MousePos = function(x, y, code) {
-    let self ={x:x, 
-		y:y, 
+    let self ={x:x,
+		y:y,
 		code:code,
 	};
-	
+
 	self.draw=function(){
 		console.log('mousepos.draw is working');
 		if(self.code='p'){
 			player.x=self.x;
 			player.y=self.y;
-			
+
 			player.update();
-			
+
 		}
 		else if(self.code='e'){
 			Enemy.generate(self.x,self.y);
@@ -35,7 +27,6 @@ MousePos = function(x, y, code) {
 	}
 	
 	return self;
->>>>>>> 95cf9af59c8136c5bd0b7cc90f1c6b21bed1cabc
 }
 
 CurrentLevel=function(){
@@ -47,8 +38,7 @@ CurrentLevel=function(){
 	};
 	//logic to send level data to to levelSave.js
 	self.save=function(){
-		
-	}
-	
-}
 
+	}
+
+}
