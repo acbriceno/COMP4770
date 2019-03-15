3123c999faa627e84f0ca888166968ee9346944e
Matt Gilchrist <mag@s1020n81h139.wst2.mun.ca>
2019-03-15 16:31:41 -0230

    conflicts

--- client/js/entity.js
+++ client/js/entity.js
@@ -61,7 +61,7 @@ Entity=function(type,id,x,y,w,h,img){
 		}
 		return testCollisionRects(rect1,rect2);
 	}
-	
+
 	self.testCollisionBB=function(rect,entity){
 		let rect2={
 			x:entity.x-entity.w/2,
@@ -71,7 +71,7 @@ Entity=function(type,id,x,y,w,h,img){
 		}
 		return testCollisionRects(rect,rect2);
 	}
-	
+
 	self.updatePosition=function(){}
 
 	Entity.list[id]=self;
@@ -99,7 +99,7 @@ Actor=function(type,id,x,y,w,h,img,hp,atkSpd,dmg,code){
 	self.weap=1;
 
 	/*self.draw=function() {
-		if(screen=='game'){	
+		if(screen=='game'){
 			ctx.save();
 		}
 		else if(screen=='le'){
@@ -111,8 +111,8 @@ Actor=function(type,id,x,y,w,h,img,hp,atkSpd,dmg,code){
 		y+=H/2;
 		x-=self.w/2;
 		y-=self.h/2;
-		
-		
+
+
 		let framew=self.img.width/4;
 		let frameh=self.img.height/28;
 		//console.log('can i find');
@@ -174,7 +174,7 @@ Actor=function(type,id,x,y,w,h,img,hp,atkSpd,dmg,code){
 		if(moveG){
 			self.y+=1;
 		}
-		
+
 		if(self.rightPress){
 			for(let key11 in Platform.list){
 				if(self.testCollisionBB(rightBump,Platform.list[key11])){
@@ -214,23 +214,21 @@ Actor=function(type,id,x,y,w,h,img,hp,atkSpd,dmg,code){
 				self.y+=self.maxSpd;
 			}
 		}
-		if(screen=='overworld'){
-			if(self.downPress){
-				for(let key11 in Platform.list){
-					if(self.testCollisionBB(downBump,Platform.list[key11])){
-						move=false;
-					}
+		if(self.downPress){
+			for(let key11 in Platform.list){
+				if(self.testCollisionBB(downBump,Platform.list[key11])){
+					move=false;
 				}
-				if(move){
-					self.y+=self.maxSpd;
-				}
-				else{
-					self.y-=self.maxSpd;
-				}
+			}
+			if(move){
+				self.y+=self.maxSpd;
+			}
+			else{
+				self.y-=self.maxSpd;
 			}
 		}
 	}
-	
+
 
 	let super_update=self.update;
 	self.update=function(){
@@ -272,7 +270,7 @@ Enemy=function(id,x,y,w,h,img,hp,atkSpd,dmg,code){
 	self.updateAim=function(){
 		var diffX = player.x - self.x;
 		var diffY = player.y - self.y;
-		
+
 		self.aimAngle = Math.atan2(diffY,diffX) / Math.PI * 180
 	}
 
@@ -285,7 +283,7 @@ Enemy=function(id,x,y,w,h,img,hp,atkSpd,dmg,code){
 		self.downPress = diffY > 3;
 		self.upPress = diffY < -3;
 	}
-		
+
 	//let super_draw=self.draw;
 	//self.draw=function(){
 	//	super_draw();
@@ -451,7 +449,7 @@ Player=function(x,y){
 	}
 
 	self.draw=function(){
-		if(screen=='game'){	
+		if(screen=='game'){
 			ctx.save();
 		}
 		else if(screen=='le'){
@@ -463,8 +461,8 @@ Player=function(x,y){
 		y+=H/2;
 		x-=self.w/2;
 		y-=self.h/2;
-		
-		
+
+
 		let framew=self.img.width/4;
 		let frameh=self.img.height/28;
 		//console.log('can i find');
@@ -506,7 +504,7 @@ Player=function(x,y){
 			ctxLE.restore();
 		}
 	}
-	
+
 	self.onDeath=function(){
 		//logic to end level on player death
 	}
@@ -608,7 +606,7 @@ Projectile.generate = function(actor){
 		Projectile(id,x,y,spdX,spdY,w,h,img1,hostile,dmg);
 	}
 	else{
-		Projectile(id,x,y,spdX,spdY,w,h,img2,hostile,dmg);	
+		Projectile(id,x,y,spdX,spdY,w,h,img2,hostile,dmg);
 	}
 }
 
