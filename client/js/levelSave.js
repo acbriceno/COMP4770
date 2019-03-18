ae69ac5be5f624dc5b6ed2235a137eaff5b145f9
Matt Gilchrist <mag@s1020n81h139.wst2.mun.ca>
2019-03-18 16:06:43 -0230

    confilces

--- client/js/levelSave.js
+++ client/js/levelSave.js
@@ -1,12 +1,12 @@
 saveLevel=function(username,playername,difficulty,progress,inventory,w,h,fac,level2d){
-	let level;
+	var level;
 	let x=0;
 	let y=0;
 	let levelString="eeep";
 
 
 	for(let i=0;i<w*h;i++){
-		let code=0;
+		let code="0";
 		for(let key in Entity.list){
 			if(Entity.list[key].x==x&&Entity.list[key].y==y){
 				code=Entity.list[key].code;
@@ -28,6 +28,7 @@ saveLevel=function(username,playername,difficulty,progress,inventory,w,h,fac,lev
 
 	console.log(updatedLevelString);
 	console.log("save Level LevelSave.js hit");
+      console.log(level)
 	return updatedLevelString;                           //return level string here
 	//logic to pass to the database
 }
