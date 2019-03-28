28816bd93a9140076c1d10215451f6d75ee59496
acbriceno <acbriceno@live.com>
2019-03-28 00:15:52 -0230

    Cheats in Campaign

---
+++ client/js/CampaignManager.js
@@ -0,0 +1,12 @@
+function checkCampaignPlayerNameCheat(playerName){
+	cheatPlayerNames = ["Bill Gates", "Mark Zuckerberg"];
+	cheat = false;
+	
+	for(var x = 0; x< cheatPlayerNames.length; x++){
+		if(cheatPlayerNames[x] == playerName){
+			cheat = true;
+			return true;
+		}
+	}
+	return cheat;
+}
