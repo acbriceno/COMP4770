ab665a1371994184064369f68a3f1bad4e463325
Matt Gilchrist <mag@Matthews-MacBook-Air.local>
2019-04-15 21:16:50 -0230

    comments for levelSave

--- client/js/levelSave.js
+++ client/js/levelSave.js
@@ -1,11 +1,13 @@
+// saveLevel.js contains two functions, saveLevel and saveLevelLE. saveLevel
+//  is used during the main campaign while saveLevelLE is used within the level
+// editor. while each take different paramaters, the main functionality is the
+// same for each. They both take a level block by block and assign a code to each.
+
 saveLevel=function(username,levelname,difficulty,w,h){
 	level="";
 	let x=0;
 	let y=0;
-
 	let fac=FAC;
-
-	console.log(fac)
 	for(let i=0;i<(w+1)*(h+1);i++){
 		let code="0";
 		for(let key in Entity.list){
@@ -35,8 +37,6 @@ saveLevel=function(username,levelname,difficulty,w,h){
 	else if(fac == 'comp'){
 		level += 'z';
 	}
-
-	console.log("level="+level);
 
 	let levelData={Username: username , Levelname : levelname , Difficulty : difficulty, W:w,H:h, Level : level
 
@@ -46,21 +46,15 @@ saveLevel=function(username,levelname,difficulty,w,h){
 }
 
 saveLevelLE=function(w,h){
-      console.log("saveLevel gets called")
 	level="";
 	let x=0;
 	let y=0;
-
 	let fac=FAC;
-
-	console.log(fac)
 	for(let i=0;i<(w+1)*(h+1);i++){
 		let code="0";
-		// console.log("x: "+x+", y: "+y);
 		for(let key in Entity.list){
 			if(Entity.list[key].x==x&&Entity.list[key].y==y){
 				code=Entity.list[key].code;
-				console.log("code : " + code);
 			}
 		}
 		level+=code;
@@ -85,8 +79,6 @@ saveLevelLE=function(w,h){
 	else if(fac == 'comp'){
 		level += 'z';
 	}
-
-	console.log("level="+level);
 
 	levelSTR=level;
 	levelW=w;
