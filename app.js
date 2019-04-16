f23bba6306aef6c53d8f0198a50aa9d8dda16e46
Alberto Briceno <acbriceno@live.com>
2019-04-15 23:07:48 -0230

    Update app.js
    Comments added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//connections to other modules
 let dbManager = require('./server/DBManager.js');
 let dbLevelManager = require('./server/DBLevelManager.js');
 var express = require('express');
@@ -11,7 +11,7 @@ let game = require('./server/GameManager.js');
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/client/index.html')
 });
-
+//serve files
 app.use("/js", express.static(__dirname + '/client/js/'));
 app.use("/img", express.static(__dirname + '/client/img/'));
 app.use("/css", express.static(__dirname + '/client/css/'));
@@ -23,8 +23,10 @@ serv.listen(2000);
 var io = require('socket.io')(serv,{});
 
 io.sockets.on('connection',function(socket){
-  console.log('socket connection');
+  //console.log('socket connection');
 
+	
+//socket connection for user signup
     socket.on('signUp', function(message){
       account.createUser(message.username, message.password, function(status){
           socket.emit('accountCreated',{
@@ -32,10 +34,12 @@ io.sockets.on('connection',function(socket){
             usernameStatus: status.usernameStatus,
           });
 		  //sconsole.log(message);
-          console.log(status);
+         // console.log(status);
       });
     });
 
+	
+//socket connection for user signin	
     socket.on('signIn', function(message){
       account.signIn(message.username, message.password, function(status){
         socket.emit('signedIn', {
@@ -45,6 +49,8 @@ io.sockets.on('connection',function(socket){
       });
       });
     });
+	
+//socket connection for user signout
 
     socket.on('signOut', function(message){
       account.signOut(message.username, message.token, function(status){
@@ -55,26 +61,29 @@ io.sockets.on('connection',function(socket){
     });
   });
 	
-	
+//socket connection for getting levels created in level editor	
 socket.on('getLevels', function(message){
       console.log("get Levels hit"); 
 	dbLevelManager.findAllLevels(function(level){
-		console.log("all levels" + level);
+		//console.log("all levels" + level);
         	socket.emit('getLevelsFound',{
 			Levels : level
           	});
 	});
   });
-	
+//socket connection for getting levels created in level editor by a certain user	
   socket.on('getUserLevels', function(message){
-        console.log("get User Levels hit");
+        //console.log("get User Levels hit");
   	dbLevelManager.findUserLevels(message.username, function(levels){
-  		console.log( levels);
+  		//console.log( levels);
           	socket.emit('userLevelsFound',{
   			Levels : levels
             	});
   	});
     });
+	
+//socket connection save level to campaign level or to level editor levels
+//distiguishes using username as "system" for campaign levels
   socket.on('saveLevel', function(message){
 	  if(message.username == 'system'){
 		  dbManager.insertSystemCourseLevel(message.levels);
@@ -84,32 +93,32 @@ socket.on('getLevels', function(message){
 	  }
   
 
-      console.log("save level server hit")
+     // console.log("save level server hit")
   });
   
-  
+  //socket connecction to load a level editor level
   socket.on('loadLevel', function(message){
-	console.log(message);
+	//console.log(message);
 	dbLevelManager.findLevel(message.username,message.levels, function(level){
-		console.log(level);
+		//console.log(level);
           	socket.emit('levelfound',{
 		Username : level.Username , Levelname : level.Levelname , Diffuculty : 	level.Difficulty, Width : level.W, Height : level.H, Faculty: level.Fac, 			Level:level.Level,
           	});
 
-      console.log("load level server hit")
+      //console.log("load level server hit")
   });
 });
-	
+	 //socket connecction to load a campaign level
 	socket.on('loadSystemCourseLevel', function(message){
 		dbManager.findSystemCourseLevel(message.courseName, function(courseLevel){
-			console.log(courseLevel);
+			//console.log(courseLevel);
 			socket.emit('systemCourseLevelFound',{
 				courseLevel : courseLevel
 			})
 		});
 	});
 	
-	
+	 //socket connecction to load a all campaign level
 	socket.on('loadAllSystemCourses', function(message){
 		dbManager.findAllSystemCourseLevels(function(Levels){
 			socket.emit('systemLevelsFound',{
@@ -120,17 +129,17 @@ socket.on('getLevels', function(message){
 	
 	
 	
-	
+ //socket connection for creating a campaign using difficulty and saving to database in user collection
   socket.on('createCampaign', function(message){
-	  console.log(message);
+	  //console.log(message);
       game.createCampaign(message.username, message.info, function(campaign){
           socket.emit('campaignCreated',{
 			  campaign : campaign
           });
-          console.log(campaign);
+          //console.log(campaign);
       });
     });
-	
+	//socket connection for getting a particular campaign belonging to a user distuguished by campaign number
 	socket.on('getCampaign', function(message){
 		game.getUserCampaign(message.username, message.campaignNumber, function(campaign){
 			socket.emit('campaign', {
@@ -139,24 +148,27 @@ socket.on('getLevels', function(message){
 		});
 	});
 	
+	//socket connection to save the current game of a campaign game play
 	socket.on('saveCurrentGame', function(message){
 		game.saveCurrentGame(message.username, message.campaignNumber, message.save.saveNumber, message.save);
 	});
 	
-	
+	//Save the current game as a new a new save file 
 	socket.on('saveNewSave', function(message){
 		game.saveNewSave(message.username, message.campaignNumber, message.save);
 	});
 	
+	//socket connection to load all campaigns belonging to a user
 	socket.on('getUserCampaigns', function (message){
 		game.getUserCampaigns(message.token, function(campaigns){
-			console.log(campaigns);
+			//console.log(campaigns);
 			socket.emit('loadUserCampaigns',{
 				campaigns : campaigns
 			});
 		});
 	});
 	
+	//socket connection to get user saves all belonging to one campaign
 	socket.on('getUserCampaignSaves', function(message) {
 			game.getUserCampaignSaves(message.token, message.campaignNumber, function(saves){
 				socket.emit('loadUserCampaignSaves', {
